7a3a1bf0999b297f1e2beb6de8a538cf69db5e20
AgungDwiPangestu <agung.dwi23@students.utdi.ac.id>
2025-12-02 22:58:09 +0700

    Rebranding: Update header from Guns Bookstore to Munif Store

---
+++ includes/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="id">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo isset($page_title) ? $page_title . ' - ' : ''; ?>Munif Store</title>
+    <link rel="stylesheet" href="/Munif/assets/css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+</head>
+
+<body>
+    <?php
+    $flash = get_flash();
+    if ($flash): ?>
+        <div class="flash-message <?php echo $flash['type']; ?>">
+            <?php echo $flash['message']; ?>
+            <span class="close-flash">&times;</span>
+        </div>
+    <?php endif; ?>
